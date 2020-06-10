0cff7d4f70b32a8ccc9c7a0b1120363aef566439
Peter Xu <peterx@redhat.com>
2020-06-10 14:27:33 -0400

    userfaultfd/shmem: Take care of UFFDIO_COPY_MODE_WP
    Apply the UFFD_WP bit properly when the UFFDIO_COPY on shmem is with
    UFFDIO_COPY_MODE_WP.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2352,9 +2352,12 @@ static int shmem_mfill_atomic_pte(struct mm_struct *dst_mm,
 		goto out_release;
 
 	_dst_pte = mk_pte(page, dst_vma->vm_page_prot);
-	if (dst_vma->vm_flags & VM_WRITE)
-		_dst_pte = pte_mkwrite(pte_mkdirty(_dst_pte));
-	else {
+	if (dst_vma->vm_flags & VM_WRITE) {
+		if (wp_copy)
+			_dst_pte = pte_mkuffd_wp(pte_wrprotect(_dst_pte));
+		else
+			_dst_pte = pte_mkwrite(pte_mkdirty(_dst_pte));
+	} else {
 		/*
 		 * We don't set the pte dirty if the vma has no
 		 * VM_WRITE permission, so mark the page dirty or it
