4b411b91bf56a1e8c453bbe7984708dcd6b0d1c0
James Houghton <jthoughton@google.com>
2022-10-04 18:14:57 +0000

    hugetlb: don't set PageUptodate for UFFDIO_CONTINUE
    If would be bad if we actually set PageUptodate with UFFDIO_CONTINUE;
    PageUptodate indicates that the page has been zeroed, and we don't want
    to give a non-zeroed page to the user.
    
    The reason this change is being made now is because UFFDIO_CONTINUEs on
    subpages definitely shouldn't set this page flag on the head page.
    
    Change-Id: I8c7ec4f1d77ad260993e037b123098bf16cfa152

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -6247,7 +6247,16 @@ int hugetlb_mcopy_atomic_pte(struct mm_struct *dst_mm,
 	 * preceding stores to the page contents become visible before
 	 * the set_pte_at() write.
 	 */
-	__SetPageUptodate(page);
+	if (!is_continue)
+		__SetPageUptodate(page);
+	else if (!PageUptodate(page)) {
+		/*
+		 * This should never happen; HugeTLB pages are always Uptodate
+		 * as soon as they are allocated.
+		 */
+		ret = -EFAULT;
+		goto out_release_nounlock;
+	}
 
 	/* Add shared, newly allocated pages to the page cache. */
 	if (vm_shared && !is_continue) {
