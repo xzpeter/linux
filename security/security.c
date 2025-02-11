64ffd61658ae538241d1e0f9bdc3631ae6e8f592
Peter Xu <peterx@redhat.com>
2025-02-11 17:03:21 -0500

    HACK/mm: EXPORT_SYMBOL()s to make KVM compile as module
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- security/security.c
+++ security/security.c
@@ -1769,6 +1769,7 @@ int security_inode_init_security_anon(struct inode *inode,
 	return call_int_hook(inode_init_security_anon, inode, name,
 			     context_inode);
 }
+EXPORT_SYMBOL(security_inode_init_security_anon);
 
 #ifdef CONFIG_SECURITY_PATH
 /**
